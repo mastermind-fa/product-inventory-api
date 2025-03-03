6e8f1f7c8f4191ee9aa61fc69be193b0bb300567
mastermind-fa <falam3399@gmail.com>
2025-03-03 22:55:26 +0600

    JWT added

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateUser = (req, res, next) => {
-  const token = req.header("Authorization");
+  const authHeader = req.header("Authorization");
 
-  if (!token) return res.status(401).json({ error: "Access denied. No token provided." });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Access denied. No valid token provided." });
+  }
+
+  const token = authHeader.split(" ")[1]; // Extract token after "Bearer"
 
   try {
-    const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
+    console.log(error);
+    
     res.status(400).json({ error: "Invalid token" });
   }
 };
